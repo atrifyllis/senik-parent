f66b1631dd618f4de0d035ba48e5307ed9814e34
Alexandros Trifyllis <alexandros.trifyllis.external@atos.net>
2023-05-24 15:36:06 +0300

    WIP add kafka connect and connector from cdk8s.

--- kube/main.ts
+++ kube/main.ts
@@ -1,27 +1,32 @@
 import {Construct} from 'constructs';
 import {App, Chart} from 'cdk8s';
-import * as strmizi from './imports/kafka.strimzi.io';
+import * as kafkaStrimzi from './imports/kafka-kafka.strimzi.io';
 import {
     KafkaSpecKafkaListenersType,
     KafkaSpecKafkaStorageType,
     KafkaSpecKafkaStorageVolumesType,
     KafkaSpecZookeeperStorageType
-} from './imports/kafka.strimzi.io';
+} from './imports/kafka-kafka.strimzi.io';
+import * as kafkaConnector from './imports/kafka-connector-kafka.strimzi.io';
+import * as kafkaConnectStrimzi from './imports/kafka-connect-kafka.strimzi.io';
 
 class MyChart extends Chart {
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        new strmizi.Kafka(this, 'kafka-cluster', {
+        const internalPort = 9092;
+        const kafka = new kafkaStrimzi.Kafka(this, 'kafka-cluster', {
             spec: {
+
                 kafka: {
                     replicas: 1,
                     listeners: [
                         {
                             name: 'plain',
-                            port: 9092,
+                            port: internalPort,
                             type: KafkaSpecKafkaListenersType.INTERNAL,
-                            tls: false
+                            tls: false,
+
                         },
                         {
                             name: 'external',
@@ -35,7 +40,8 @@ class MyChart extends Chart {
                                 brokers: [
                                     {
                                         broker: 0,
-                                        host: 'broker-0.kubernetes.docker.internal' // TODO not sure where this is needed
+                                        host: 'broker-0.kubernetes.docker.internal', // TODO not sure where this is needed
+
                                     }
                                 ]
                             }
@@ -65,6 +71,55 @@ class MyChart extends Chart {
                 }
             }
         });
+
+        new kafkaConnectStrimzi.KafkaConnect(this, 'kafka-connect-cluster', {
+            metadata: {
+                annotations: {
+                    'strimzi.io/use-connector-resources': 'true' // needed to enable kafka connectors
+                }
+            },
+            spec: {
+                image: 'debezium/connect:latest',
+                replicas: 1,
+                bootstrapServers: `${kafka.name}-kafka-bootstrap:${internalPort}`,
+                config: {
+                    'group.id': 'senik-debezium-connect-group',
+                    'offset.storage.topic': 'senik-debezium-offsets',
+                    'config.storage.topic': 'senik-debezium-config',
+                    'status.storage.topic': 'senik-debezium-status'
+                }
+            }
+        });
+
+        new kafkaConnector.KafkaConnector(this, 'postgres-sink-kafka-connector', {
+            metadata: {
+                name: 'senik-outbox-connector'
+            },
+            spec: {
+                config: {
+                    'connector.class': 'io.debezium.connector.postgresql.PostgresConnector',
+                    'database.hostname': 'db',
+                    'database.port': '5432',
+                    'database.user': 'senik',
+                    'database.password': 'senik',
+                    'database.dbname': 'senik',
+                    'database.server.name': 'db',
+                    'key.converter': 'org.apache.kafka.connect.json.JsonConverter',
+                    'key.converter.schemas.enable': 'false',
+                    'value.converter': 'org.apache.kafka.connect.json.JsonConverter',
+                    'value.converter.schemas.enable': 'false',
+                    'tombstones.on.delete': 'false',
+                    'table.include.list': 'public.persisted_event',
+                    'topic.prefix': 'senik',
+                    'transforms': 'outbox',
+                    'transforms.outbox.type': 'io.debezium.transforms.outbox.EventRouter',
+                    'transforms.outbox.table.expand.json.payload': 'true',
+                    'transforms.outbox.route.topic.replacement': 'senik.events',
+                    'transforms.outbox.table.fields.additional.placement': 'tracingspancontext:header:traceparent'
+                }
+            }
+        });
+
     }
 }
 
