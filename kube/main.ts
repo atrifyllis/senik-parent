4fee647d1935849c11dec1c16f3d8d78e714d480
Alexandros Trifyllis <alexandros.trifyllis.external@atos.net>
2023-06-01 17:20:37 +0300

    Fix strimzi kafka metrics and consequently grafana dashboard.

--- kube/main.ts
+++ kube/main.ts
@@ -17,12 +17,13 @@ import * as kplus from 'cdk8s-plus-25';
 import {EnvValue, HttpIngressPathType, PersistentVolumeAccessMode, ServiceType} from 'cdk8s-plus-25';
 import {ChartProps} from "cdk8s/lib/chart";
 
-
 const SENIK_DB_PORT = 5432;
 const SENIK_DB_NODE_PORT = 30020;
 
 const KAFKA_INTERNAL_PORT = 9092;
 const KAFKA_NODE_PORT = 30019;
+const KAFKA_METRICS_CONFIG_KEY = 'kafka-metrics-config.yaml';
+
 const KAFKA_UI_LOCAL_ADDRESS = 'kafka-ui.127.0.0.1.nip.io';
 
 export class MyChart extends Chart {
@@ -71,11 +72,137 @@ export class MyChart extends Chart {
             serviceType: ServiceType.NODE_PORT
         });
 
-        // this config map is only needed by strimzi to enable prometheus. what a mess...
-        // From strimzi docs: "You can enable metrics without further configuration using a reference to a ConfigMap
-        // containing an empty file under metricsConfig.valueFrom.configMapKeyRef.key."
-        let dummyKafkaConfigMap = new kplus.ConfigMap(this, 'dummy-configmap', {});
-        dummyKafkaConfigMap.addFile('empty-file.yaml', 'dummy');
+        // without the rules in this configmap the strimzi-kafka dashboard does not work!
+        let kafkaPrometheusConfigMap = new kplus.ConfigMap(this, 'kafka-prom-configmap', {});
+        kafkaPrometheusConfigMap.addData(KAFKA_METRICS_CONFIG_KEY, `
+            
+        # unfortunately, an empty file did not work
+        lowercaseOutputName: true
+        lowercaseOutputLabelNames: true # without it, kafka metrics are snake case and grafana dashboards won't work
+        rules:
+          # Special cases and very specific rules
+          - pattern: kafka.server<type=(.+), name=(.+), clientId=(.+), topic=(.+), partition=(.*)><>Value
+            name: kafka_server_$1_$2
+            type: GAUGE
+            labels:
+              clientId: "$3"
+              topic: "$4"
+              partition: "$5"
+          - pattern: kafka.server<type=(.+), name=(.+), clientId=(.+), brokerHost=(.+), brokerPort=(.+)><>Value
+            name: kafka_server_$1_$2
+            type: GAUGE
+            labels:
+              clientId: "$3"
+              broker: "$4:$5"
+          - pattern: kafka.server<type=(.+), cipher=(.+), protocol=(.+), listener=(.+), networkProcessor=(.+)><>connections
+            name: kafka_server_$1_connections_tls_info
+            type: GAUGE
+            labels:
+              cipher: "$2"
+              protocol: "$3"
+              listener: "$4"
+              networkProcessor: "$5"
+          - pattern: kafka.server<type=(.+), clientSoftwareName=(.+), clientSoftwareVersion=(.+), listener=(.+), networkProcessor=(.+)><>connections
+            name: kafka_server_$1_connections_software
+            type: GAUGE
+            labels:
+              clientSoftwareName: "$2"
+              clientSoftwareVersion: "$3"
+              listener: "$4"
+              networkProcessor: "$5"
+          - pattern: "kafka.server<type=(.+), listener=(.+), networkProcessor=(.+)><>(.+):"
+            name: kafka_server_$1_$4
+            type: GAUGE
+            labels:
+              listener: "$2"
+              networkProcessor: "$3"
+          - pattern: kafka.server<type=(.+), listener=(.+), networkProcessor=(.+)><>(.+)
+            name: kafka_server_$1_$4
+            type: GAUGE
+            labels:
+              listener: "$2"
+              networkProcessor: "$3"
+          # Some percent metrics use MeanRate attribute
+          # Ex) kafka.server<type=(KafkaRequestHandlerPool), name=(RequestHandlerAvgIdlePercent)><>MeanRate
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)Percent\\w*><>MeanRate
+            name: kafka_$1_$2_$3_percent
+            type: GAUGE
+          # Generic gauges for percents
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)Percent\\w*><>Value
+            name: kafka_$1_$2_$3_percent
+            type: GAUGE
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)Percent\\w*, (.+)=(.+)><>Value
+            name: kafka_$1_$2_$3_percent
+            type: GAUGE
+            labels:
+              "$4": "$5"
+          # Generic per-second counters with 0-2 key/value pairs
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)PerSec\\w*, (.+)=(.+), (.+)=(.+)><>Count
+            name: kafka_$1_$2_$3_total
+            type: COUNTER
+            labels:
+              "$4": "$5"
+              "$6": "$7"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)PerSec\\w*, (.+)=(.+)><>Count
+            name: kafka_$1_$2_$3_total
+            type: COUNTER
+            labels:
+              "$4": "$5"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)PerSec\\w*><>Count
+            name: kafka_$1_$2_$3_total
+            type: COUNTER
+          # Generic gauges with 0-2 key/value pairs
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+), (.+)=(.+), (.+)=(.+)><>Value
+            name: kafka_$1_$2_$3
+            type: GAUGE
+            labels:
+              "$4": "$5"
+              "$6": "$7"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+), (.+)=(.+)><>Value
+            name: kafka_$1_$2_$3
+            type: GAUGE
+            labels:
+              "$4": "$5"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)><>Value
+            name: kafka_$1_$2_$3
+            type: GAUGE
+          # Emulate Prometheus 'Summary' metrics for the exported 'Histogram's.
+          # Note that these are missing the '_sum' metric!
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+), (.+)=(.+), (.+)=(.+)><>Count
+            name: kafka_$1_$2_$3_count
+            type: COUNTER
+            labels:
+              "$4": "$5"
+              "$6": "$7"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+), (.+)=(.*), (.+)=(.+)><>(\\d+)thPercentile
+            name: kafka_$1_$2_$3
+            type: GAUGE
+            labels:
+              "$4": "$5"
+              "$6": "$7"
+              quantile: "0.$8"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+), (.+)=(.+)><>Count
+            name: kafka_$1_$2_$3_count
+            type: COUNTER
+            labels:
+              "$4": "$5"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+), (.+)=(.*)><>(\\d+)thPercentile
+            name: kafka_$1_$2_$3
+            type: GAUGE
+            labels:
+              "$4": "$5"
+              quantile: "0.$6"
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)><>Count
+            name: kafka_$1_$2_$3_count
+            type: COUNTER
+          - pattern: kafka.(\\w+)<type=(.+), name=(.+)><>(\\d+)thPercentile
+            name: kafka_$1_$2_$3
+            type: GAUGE
+            labels:
+              quantile: "0.$4"
+
+            
+        `)
 
         const kafka = new kafkaStrimzi.Kafka(this, 'kafka-cluster', {
             spec: {
@@ -125,8 +252,8 @@ export class MyChart extends Chart {
                         type: KafkaSpecKafkaMetricsConfigType.JMX_PROMETHEUS_EXPORTER,
                         valueFrom: {
                             configMapKeyRef: {
-                                name: dummyKafkaConfigMap.name,
-                                key: 'dummy'
+                                name: kafkaPrometheusConfigMap.name,
+                                key: KAFKA_METRICS_CONFIG_KEY
                             }
                         },
 
