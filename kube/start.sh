855482d8b0c61ef6e572de01248e5058bbdd6577
otinanism <otinanism@gmail.com>
2023-06-06 21:11:42 +0300

    Add start bash script to start all components in k8s.

---
+++ kube/start.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+helm install prom prometheus-community/kube-prometheus-stack --values ../obs/prom-stack-values.yaml
+
+helm install strimzi-operator strimzi/strimzi-kafka-operator
+
+kubectl apply -f strimzi-pod-monitor.yaml
+
+helm install loki grafana/loki --values ../obs/loki-values.yaml
+
+kubectl apply -f dist
